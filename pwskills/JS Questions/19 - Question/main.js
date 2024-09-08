67eba2238eeabe07cf07f81885fd765ba5326c25
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2024-09-08 15:54:01 +0530

    add some assignment

---
+++ pwskills/JS Questions/19 - Question/main.js
@@ -0,0 +1,8 @@
+// In Node.js, JavaScript engine uses the commonJS environment which uses the require() function to import module
+
+const countries = require('./countries.js');
+const webTechs = require('./web_techs.js');
+
+//Accessing variables from countries.js & web_Tech.js
+console.log(countries);
+console.log(webTechs);
---
+++ pwskills/JS Questions/19 - Question/web_techs.js
@@ -0,0 +1,11 @@
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React JS',
+  'Node JS',
+  'MongoDB',
+];
+
+// exporting WebTechs variable so that we can import them in other files
+module.exports = webTechs;
